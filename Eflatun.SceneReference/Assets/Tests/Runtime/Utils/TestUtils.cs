74d28f98cf1c9a6aadf454858d5fe463a34b92c9
starikcetin <cetinsamedtarik@gmail.com>
2026-03-01 20:44:56 +0300

    tests: use GetRawBinaryBase64 instead of hardcoding base64 values
    Closes https://github.com/starikcetin/Eflatun.SceneReference/issues/122

--- Eflatun.SceneReference/Assets/Tests/Runtime/Utils/TestUtils.cs
+++ Eflatun.SceneReference/Assets/Tests/Runtime/Utils/TestUtils.cs
@@ -25,7 +25,7 @@ namespace Eflatun.SceneReference.Tests.Runtime.Utils
         public const string EnabledSceneGuid = "e3f2c1473b766c34ba5b37779d71787e";
         public static string EnabledSceneJsonRaw => GetRawJson(EnabledSceneGuid);
         public static string EnabledSceneXmlRaw => GetRawXml(EnabledSceneGuid);
-        public const string EnabledSceneBinaryBase64 = @"AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACBlM2YyYzE0NzNiNzY2YzM0YmE1YjM3Nzc5ZDcxNzg3ZQs=";
+        public static string EnabledSceneBinaryBase64 => GetRawBinaryBase64(EnabledSceneGuid);
 
         public const string DisabledSceneName = "TestSubject_Disabled";
         public const string DisabledScenePath = "Assets/Tests/Runtime/Subjects/TestSubject_Disabled.unity";
@@ -33,7 +33,7 @@ namespace Eflatun.SceneReference.Tests.Runtime.Utils
         public const string DisabledSceneGuid = "7e37b14fa3517514a91937cec5cad27a";
         public static string DisabledSceneJsonRaw => GetRawJson(DisabledSceneGuid);
         public static string DisabledSceneXmlRaw => GetRawXml(DisabledSceneGuid);
-        public const string DisabledSceneBinaryBase64 = @"AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACA3ZTM3YjE0ZmEzNTE3NTE0YTkxOTM3Y2VjNWNhZDI3YQs=";
+        public static string DisabledSceneBinaryBase64 => GetRawBinaryBase64(DisabledSceneGuid);
 
         public const string NotInBuildSceneName = "TestSubject_NotInBuild";
         public const string NotInBuildScenePath = "Assets/Tests/Runtime/Subjects/TestSubject_NotInBuild.unity";
@@ -41,28 +41,28 @@ namespace Eflatun.SceneReference.Tests.Runtime.Utils
         public const string NotInBuildSceneGuid = "63c386231869c904c9b701dd79268476";
         public static string NotInBuildSceneJsonRaw => GetRawJson(NotInBuildSceneGuid);
         public static string NotInBuildSceneXmlRaw => GetRawXml(NotInBuildSceneGuid);
-        public const string NotInBuildSceneBinaryBase64 = @"AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACA2M2MzODYyMzE4NjljOTA0YzliNzAxZGQ3OTI2ODQ3Ngs=";
+        public static string NotInBuildSceneBinaryBase64 => GetRawBinaryBase64(NotInBuildSceneGuid);
 
         public const string AllZeroGuid = "00000000000000000000000000000000";
         public static string EmptyReferenceJsonRaw => GetRawJson(AllZeroGuid);
         public static string EmptyReferenceXmlRaw => GetRawXml(AllZeroGuid);
-        public const string EmptyReferenceBinaryBase64 = @"AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMAs=";
+        public static string EmptyReferenceBinaryBase64 => GetRawBinaryBase64(AllZeroGuid);
 
         public const string DeletedSceneGuid = "69c1683d94db0cc469d86e4e865f9f5d";
         public static string DeletedSceneJsonRaw => GetRawJson(DeletedSceneGuid);
         public static string DeletedSceneXmlRaw => GetRawXml(DeletedSceneGuid);
-        public const string DeletedSceneBinaryBase64 = "AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACA2OWMxNjgzZDk0ZGIwY2M0NjlkODZlNGU4NjVmOWY1ZAs=";
+        public static string DeletedSceneBinaryBase64 => GetRawBinaryBase64(DeletedSceneGuid);
 
         public const string NotExistingGuid = "2bc1683d94d80cc269d85e4e8a5fcf5d";
         public static string NotExistingJsonRaw => GetRawJson(NotExistingGuid);
         public static string NotExistingXmlRaw => GetRawXml(NotExistingGuid);
-        public const string NotExistingBinaryBase64 = "AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACAyYmMxNjgzZDk0ZDgwY2MyNjlkODVlNGU4YTVmY2Y1ZAs=";
+        public static string NotExistingBinaryBase64 => GetRawBinaryBase64(NotExistingGuid);
 
         public const string NotSceneAssetGuid = "99d2aa5f58f54c44fba8671b66be5259";
         public const string NotSceneAssetPath = "Assets/Tests/Runtime/Utils/TestSubject_Material.mat";
         public static string NotSceneAssetJsonRaw => GetRawJson(NotSceneAssetGuid);
         public static string NotSceneAssetXmlRaw => GetRawXml(NotSceneAssetGuid);
-        public const string NotSceneAssetBinaryBase64 = "AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACA5OWQyYWE1ZjU4ZjU0YzQ0ZmJhODY3MWI2NmJlNTI1OQs=";
+        public static string NotSceneAssetBinaryBase64 => GetRawBinaryBase64(NotSceneAssetGuid);
 
         public const string Addressable1SceneName = "TestSubject_Addressable1";
         public const string Addressable1ScenePath = "Assets/Tests/Runtime/Subjects/TestSubject_Addressable1.unity";
@@ -70,7 +70,7 @@ namespace Eflatun.SceneReference.Tests.Runtime.Utils
         public const string Addressable1SceneGuid = "8b3f523138018e04ebcb86e1230451b1";
         public static string Addressable1SceneJsonRaw => GetRawJson(Addressable1SceneGuid);
         public static string Addressable1SceneXmlRaw => GetRawXml(Addressable1SceneGuid);
-        public const string Addressable1SceneBinaryBase64 = "AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACA4YjNmNTIzMTM4MDE4ZTA0ZWJjYjg2ZTEyMzA0NTFiMQs=";
+        public static string Addressable1SceneBinaryBase64 => GetRawBinaryBase64(Addressable1SceneGuid);
         public const string Addressable1SceneAddress = "Test Subject Addressable1";
 
         public const string Addressable2SceneName = "TestSubject_Addressable2";
@@ -79,7 +79,7 @@ namespace Eflatun.SceneReference.Tests.Runtime.Utils
         public const string Addressable2SceneGuid = "32b8f3692a793ae4693d74167b0f093f";
         public static string Addressable2SceneJsonRaw => GetRawJson(Addressable2SceneGuid);
         public static string Addressable2SceneXmlRaw => GetRawXml(Addressable2SceneGuid);
-        public const string Addressable2SceneBinaryBase64 = "AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACAzMmI4ZjM2OTJhNzkzYWU0NjkzZDc0MTY3YjBmMDkzZgs=";
+        public static string Addressable2SceneBinaryBase64 => GetRawBinaryBase64(Addressable2SceneGuid);
         public const string Addressable2SceneAddress = "Test Subject Addressable2";
 
         public const string AddressableDuplicateAddressASceneName = "TestSubject_AddressableDuplicateAddressA";
@@ -88,7 +88,7 @@ namespace Eflatun.SceneReference.Tests.Runtime.Utils
         public const string AddressableDuplicateAddressASceneGuid = "97f5e006e871a7440b0ffe04cb128c15";
         public static string AddressableDuplicateAddressASceneJsonRaw => GetRawJson(AddressableDuplicateAddressASceneGuid);
         public static string AddressableDuplicateAddressASceneXmlRaw => GetRawXml(AddressableDuplicateAddressASceneGuid);
-        public const string AddressableDuplicateAddressASceneBinaryBase64 = "AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACA5N2Y1ZTAwNmU4NzFhNzQ0MGIwZmZlMDRjYjEyOGMxNQs=";
+        public static string AddressableDuplicateAddressASceneBinaryBase64 => GetRawBinaryBase64(AddressableDuplicateAddressASceneGuid);
         public const string AddressableDuplicateAddressASceneAddress = "Test Subject AddressableDuplicateAddress";
 
         public const string AddressableDuplicateAddressBSceneName = "TestSubject_AddressableDuplicateAddressB";
@@ -97,7 +97,7 @@ namespace Eflatun.SceneReference.Tests.Runtime.Utils
         public const string AddressableDuplicateAddressBSceneGuid = "c367bc2503743ea4b8abccc44b2fefbc";
         public static string AddressableDuplicateAddressBSceneJsonRaw => GetRawJson(AddressableDuplicateAddressBSceneGuid);
         public static string AddressableDuplicateAddressBSceneXmlRaw => GetRawXml(AddressableDuplicateAddressBSceneGuid);
-        public const string AddressableDuplicateAddressBSceneBinaryBase64 = "AAEAAAD/////AQAAAAAAAAAMAgAAAE1FZmxhdHVuLlNjZW5lUmVmZXJlbmNlLCBWZXJzaW9uPTAuMC4wLjAsIEN1bHR1cmU9bmV1dHJhbCwgUHVibGljS2V5VG9rZW49bnVsbAUBAAAAJUVmbGF0dW4uU2NlbmVSZWZlcmVuY2UuU2NlbmVSZWZlcmVuY2UBAAAAEXNjZW5lQXNzZXRHdWlkSGV4AQIAAAAGAwAAACBjMzY3YmMyNTAzNzQzZWE0YjhhYmNjYzQ0YjJmZWZiYws=";
+        public static string AddressableDuplicateAddressBSceneBinaryBase64 => GetRawBinaryBase64(AddressableDuplicateAddressBSceneGuid);
         public const string AddressableDuplicateAddressBSceneAddress = "Test Subject AddressableDuplicateAddress";
 
         public const string NonExistingAddress = "This Address Should Never Exist ___ Foo ___ Bar";
